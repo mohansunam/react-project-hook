9beae5ed08aa19c18a0a2b97854788e0fe46ef70
mohan <mohansunam234@gmail.com>
2024-07-27 14:42:23 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,69 +1,35 @@
-import { Button, Input } from '@material-tailwind/react'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { Button, Input } from '@material-tailwind/react';
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
 
 const App = () => {
-  const [data, setData]= useState();
-  const getData = async() =>{
-    try {
-      const response = await axios.get('https://dummyjson.com/recipes');
-      setData(response.data);
-
-    } catch(err){};
-  }
-  useEffect(() =>{
-    getData();
-  }, [])
+  const [user, setUser]= useState([])
+  const formik = useFormik({
+    initialValues:{
+      email:''
+    },
+    onSubmit: (val)=>{
+      setUser((pre)=>[...pre, val.email]);
+    }
+  });
+  console.log(user)
   return (
-    <div>
-      {data && <div className='grid grid-cols-6 gap-y-4 gap-x-2'>
-        {data.recipes.map((product)=>{
-          return <div className='w-[100%] ' key={product.id}>
-             <img src={product.image} alt="" />
-             <h1 className='font-bold'>{product.name}</h1>
-             <h1>{product.ingredients.map((item, i)=>{
-              return <p key={i}>{`${i+1}.`}{item}</p>
-             })}</h1>
-          </div>
-        })
-        }
-
-      </div>
-      }
+    <div className='p-8 space-y-3' >
+      <form onSubmit={formik.handleSubmit}>
+        <Input 
+        name='email'
+        onChange={formik.handleChange}
+        label='Email'
+        value={formik.values.email}
+      
+        placeholder='@gamil.com'
+        /> 
+        <Button type='submit'>Submit</Button>
+      </form>
+   
       
     </div>
   )
 }
 
 export default App
-
-
-
-
-{/* <div className='w-[100%] '>
-      { data && <div className=' grid grid-cols-6 gap-x-2 gap-y-2'>
-        {data.recipes.map((recipe)=>{
-          return <div key={recipe.id}>
-            <img src={recipe.image} alt="" />
-            <h1>{recipe.ingredients.map((ing, i)=> {
-              return <p key={i}> {`${i+1}.`}{ing}</p>
-
-            })}</h1>
-
-          </div>
-        })}
-      </div>
-
-      }
-    </div> */}
-   
-
-
-{/* <div className='p-8  flex justify-end gap-3'> 
-<form className=''>
-<Input className='w-[300px] text-red-500' placeholder='Movie Name' type="text" />
-
-</form>
-
-  <Button className='p-2 '>Search movie</Button>
-</div> */}
